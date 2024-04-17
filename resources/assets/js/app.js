0d30ddf8eda4c67ced8272d7807ac4f5aa45e1b1
ArnasPip <pipinisarnas@gmail.com>
2024-04-17 03:20:15 +0300

    js package and bootstrap css set up

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import flatpickr from "flatpickr";
+
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelectorAll('.datepicker').forEach(function (item){
+        flatpickr(item, {
+            mode: 'range'
+        });
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,26 @@
 <head
     <meta charset="UTF-8">
     <meta name="viewport"
-         content="witdh=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+<header>
+    <form action="#" method="POST">
+        <input type="text" class="datepicker">
+    </form>
+    <div class="container">
+        <h1 class="text-center">Bootstrap Test</h1>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <p class="text-center">If you see this text styled with Bootstrap, then Bootstrap is set up correctly.</p>
+                <button class="btn btn-primary btn-block">Button</button>
+            </div>
+        </div>
+    </div>
+</header>
 <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
